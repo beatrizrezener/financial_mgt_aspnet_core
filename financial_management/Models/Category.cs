3cd16de535641139f2404807a90a3a9f9bd85709
Beatriz Rezener <beatrizrezener@gmail.com>
2020-10-18 20:47:38 -0300

    Initial asp.net core MVC project + entity framework

---
+++ financial_management/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace financial_management.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+        }
+    }
+}
---
+++ financial_management/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace financial_management.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
